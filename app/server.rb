63384c4adb60b876fb7b858b5a2503b7fafac561
Maya Driver <mayadrivermd@gmail.com>
2014-09-15 22:32:36 +0100

    added user sign in

--- app/server.rb
+++ app/server.rb
@@ -1,5 +1,6 @@
 require 'sinatra'
 require 'data_mapper'
+require 'rack-flash'
 
 require './lib/peep'
 require './lib/user'
@@ -8,6 +9,7 @@ require_relative 'data_mapper_setup'
 
 enable :sessions
 set :session_secret, 'super secret'
+use Rack::Flash
 
 get '/' do
 	@peeps = Peep.all
@@ -21,12 +23,34 @@ post '/peeps' do
 end
 
 get '/users/new' do
+	@user = User.new
 	erb :"users/new"
 end
 
 post '/users' do
-	user = User.create(:email => params[:email],
+	@user = User.new(:email => params[:email],
 				:password => params[:password])
-	session[:user_id] = user.id
-	redirect to('/')
+	if @user.save
+		session[:user_id] = @user.id
+		redirect to('/')
+	else
+		flash.now[:errors] = @user.errors.full_messages
+		erb :"users/new"
+	end
+end
+
+get '/sessions/new' do
+	erb :"sessions/new"
+end
+
+post '/sessions' do
+	email, password = params[:email], params[:password]
+	user = User.authenticate(email, password)
+	if user
+		session[:user_id] = user.id
+		redirect to ('/')
+	else
+		flash[:errors] = ["The email or password is incorrect"]
+			erb :"sessions/new"
+	end
 end
--- spec/features/user_spec.rb
+++ spec/features/user_spec.rb
@@ -18,3 +18,34 @@ feature "User signing up" do
 	end
 
 end
+
+feature "User signs in" do
+
+	before(:each) do
+		User.create(:email => "test@test.com",
+					:password => 'test')
+	end
+
+	scenario "with correct credentials" do
+		visit '/'
+		expect(page).not_to have_content("Hello, test@test.com")
+		sign_in('test@test.com', 'test')
+		expect(page).to have_content("Hello, test@test.com")
+	end
+
+	scenario "with incorrect credentials" do
+		visit '/'
+		expect(page).not_to have_content("Hello, test@test.com")
+		sign_in('test@test.com', 'wrong')
+		expect(page).not_to have_content("Hello, test@test.com")
+	end
+
+	def sign_in(email, password)
+		visit '/sessions/new'
+		fill_in 'email', :with => email
+		fill_in 'password', :with => password
+		click_button 'Sign in'
+	end
+
+
+end
